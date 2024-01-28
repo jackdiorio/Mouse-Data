1b9731590746101c8c288f1b7184228d02cdaa9e
Jack C. D'Iorio <73403239+jackdiorio@users.noreply.github.com>
2024-01-27 19:24:32 -0500

    changed example to test get_punnett function

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import datetime
 from Mouse import Mouse # from (Mouse.py file) import (the class Mouse itself)
 
 # Keeping example from google sheet (thank you tommy its very well done!)
-jesus = Mouse("Jesus", datetime.date(2023, 12, 25), "none", "none", "black", "a", "a")
+jesus = Mouse("Jesus", datetime.date(2023, 12, 25), "none", "none", "black", "A", "a")
 ophelia = Mouse("Ophelia", datetime.date(2023, 11, 20), "none", "none", "brindle", "Avy", "a")
 
 # Mate them and get punnet square for locus A (only one supported as of yet)
